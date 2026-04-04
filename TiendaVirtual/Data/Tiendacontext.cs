7afef77357377aa289a20d291bdbb5c8bcfa47cf
xbox game store <xboxgamestore11@gmail.com>
2026-04-04 09:52:06 -0500

    Taller CRUD Usuarios completo

--- TiendaVirtual/Data/Tiendacontext.cs
+++ TiendaVirtual/Data/Tiendacontext.cs
@@ -13,5 +13,18 @@ namespace TiendaVirtual.Data
         public DbSet<Producto> Productos { get; set; }
         public DbSet<Categoria> Categorias { get; set; }
         public DbSet<Usuario> Usuarios { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Usuario>().HasData(
+                new Usuario { Id = 1, Nombre = "Carlos Ramírez", Correo = "admin@tienda.com", Rol = "Administrador", celular = "3001234567" },
+                new Usuario { Id = 2, Nombre = "Laura Gómez", Correo = "cajero@tienda.com", Rol = "Cajero", celular = "3109876543" },
+                new Usuario { Id = 3, Nombre = "Andrés Torres", Correo = "bodeguero@tienda.com", Rol = "Bodeguero", celular = "3154561234" },
+                new Usuario { Id = 4, Nombre = "María Pérez", Correo = "cliente1@correo.com", Rol = "Cliente", celular = "3201112233" },
+                new Usuario { Id = 5, Nombre = "Juan Mora", Correo = "cliente2@correo.com", Rol = "Cliente", celular = "3253334455" }
+            );
+        }
     }
 }
