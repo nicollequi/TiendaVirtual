f5714ae59fa2dc123244f54ca713f75c4527c832
Nicolle <nico@email.com>
2026-04-13 11:42:32 -0500

    semana 8 boton

--- TiendaVirtual/Controllers/ProductoController.cs
+++ TiendaVirtual/Controllers/ProductoController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using TiendaVirtual.Data;
 using TiendaVirtual.Models;
@@ -20,6 +21,57 @@ namespace TiendaVirtual.Controllers
                 .Include(p => p.Categoria)
                 .ToListAsync();
             return View(productos);
+        }
+
+        // GET
+        public IActionResult Create()
+        {
+            ViewBag.CategoriaId = new SelectList(_context.Categorias, "Id", "Nombre");
+            return View();
+        }
+
+        // POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Producto producto)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(producto);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewBag.CategoriaId = new SelectList(_context.Categorias, "Id", "Nombre");
+            return View(producto);
+        }
+
+        // GET
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null) return NotFound();
+
+            var producto = await _context.Productos.FindAsync(id);
+            if (producto == null) return NotFound();
+
+            ViewBag.CategoriaId = new SelectList(_context.Categorias, "Id", "Nombre", producto.CategoriaId);
+            return View(producto);
+        }
+
+        // POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, Producto producto)
+        {
+            if (id != producto.Id) return NotFound();
+
+            if (ModelState.IsValid)
+            {
+                _context.Update(producto);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewBag.CategoriaId = new SelectList(_context.Categorias, "Id", "Nombre", producto.CategoriaId);
+            return View(producto);
         }
     }
 }
